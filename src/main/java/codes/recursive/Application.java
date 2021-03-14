54c848742cc2256a0ff46b3169cb36423f719647
recursivecodes <toddraymondsharp@gmail.com>
2021-03-13 20:24:51 -0500

    set timezone to New_York

--- src/main/java/codes/recursive/Application.java
+++ src/main/java/codes/recursive/Application.java
@@ -2,10 +2,13 @@ package codes.recursive;
 
 import io.micronaut.runtime.Micronaut;
 
+import java.util.TimeZone;
+
 public class Application {
 
     public static void main(String[] args) {
         System.setProperty("oracle.jdbc.fanEnabled", "false");
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
         Micronaut.run(Application.class, args);
     }
 }
